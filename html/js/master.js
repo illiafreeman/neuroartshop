487d59ff611a4a62972f557f617596b49711b4dc
freeman <iviewn@gmail.com>
2022-07-23 15:51:41 +0300

    index

--- html/js/master.js
+++ html/js/master.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-    $('.slider').slick({
-      dots: true
-    });
-  });
+	$('.slider').slick({
+		dots: true
+	});
+	$('.popular-products-group').slick({
+		infinite: true,
+  		slidesToShow: 4,
+		slidesToScroll: 1
+	});
+	$('.team-slider').slick({
+		infinite: true,
+  		slidesToShow: 3,
+		slidesToScroll: 1
+	});
+});
