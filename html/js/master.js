c4717ed59c22a155d9fd9ddb769834994e144895
freeman <iviewn@gmail.com>
2022-07-26 10:46:46 +0300

    index

--- html/js/master.js
+++ html/js/master.js
@@ -23,71 +23,18 @@ $(document).ready(function(){
         $(this).removeClass('vis');
 		$('.hambu').addClass('vis');
     });
+	$('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
 
 
 
 
 
-	$('.slider').slick({
-		dots: true,
-		responsive: [
-			{
-				breakpoint: 600,
-				settings: {
-				  arrows: false
-				}
-			  }
-		]
-	});
-	$('.popular-products-group').slick({
-		infinite: true,
-  		slidesToShow: 4,
-		slidesToScroll: 4,
-		responsive: [
-			{
-			  breakpoint: 960,
-			  settings: {
-				centerMode: true,
-				arrows: false,
-				slidesToShow: 3
-			  }
-			},
-			{
-			  breakpoint: 600,
-			  settings: {
-				arrows: false,
-				centerMode: true,
-				slidesToShow: 2
-			  }
-			},
-			{
-				breakpoint: 480,
-				settings: {
-				  arrows: false,
-				  centerMode: true,
-				  slidesToShow: 1
-				}
-			  }
-		]
-	});
-	$('.team-slider').slick({
-		infinite: true,
-  		slidesToShow: 3,
-		slidesToScroll: 1,
-		responsive: [
-			{
-			  breakpoint: 960,
-			  settings: {
-				//centerMode: true,
-				slidesToShow: 2
-			  }
-			},
-			{
-				breakpoint: 768,
-				settings: {
-				  slidesToShow: 1
-				}
-			}
-		]
-	});
+
 });
