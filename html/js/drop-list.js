35310e6503bf50e0eec89f93f625aea047a50d30
freeman <iviewn@gmail.com>
2022-07-26 19:31:14 +0300

    dostavka

--- html/js/drop-list.js
+++ html/js/drop-list.js
@@ -4,8 +4,8 @@ console.log(drop_list_title);
 
 for(let i = 0; drop_list_title.length; i++) {
     drop_list_title[i].onclick = function() {
-        if(drop_list_title[i].nextElementSibling.style.display != 'grid') {
-            drop_list_title[i].nextElementSibling.style.display = 'grid';
+        if(drop_list_title[i].nextElementSibling.style.display != 'block') {
+            drop_list_title[i].nextElementSibling.style.display = 'block';
         }
         else {
             drop_list_title[i].nextElementSibling.style.display = 'none';
