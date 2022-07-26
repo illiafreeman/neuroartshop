15c7f3ac53518df1d936c81990ace19ca4a30801
freeman <iviewn@gmail.com>
2022-07-26 14:47:14 +0300

    cart

--- html/js/master.js
+++ html/js/master.js
@@ -23,14 +23,7 @@ $(document).ready(function(){
         $(this).removeClass('vis');
 		$('.hambu').addClass('vis');
     });
-	$('.minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-    });
+
 
 
 
